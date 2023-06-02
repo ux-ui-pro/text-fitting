a70e29490c1f0913de12acc8603b9938c77d255b
ux-ui-pro <maxim@ux-ui.pro>
2023-06-02 21:40:47 +0300

    issue: added the destroy and init methods

--- dist/index.module.js
+++ dist/index.module.js
@@ -4,28 +4,53 @@ class $cf838c15c8b009ba$export$2e2bcd8739ae039 extends HTMLElement {
         const shadowOpen = this.attachShadow({
             mode: "open"
         });
-        shadowOpen.innerHTML = `<div class="wrap" style="display: flex; justify-content: center;"><div class="body" style="white-space: nowrap;"><slot></slot></div></div>`;
+        shadowOpen.innerHTML = `<div class="wrap"><div class="body"><slot></slot></div></div>`;
         this.wrap = shadowOpen.querySelector(".wrap");
         this.body = shadowOpen.querySelector(".body");
         this.update = this.update.bind(this);
         this.af = null;
         this.resizeObserver = new ResizeObserver(this.update);
-        this.resizeObserver.observe(this.wrap);
+    }
+    init() {
         document.fonts.addEventListener("loadingdone", this.update);
+        this.resizeObserver.observe(this.wrap);
     }
     update() {
         if (this.af) cancelAnimationFrame(this.af);
         this.af = requestAnimationFrame(()=>{
-            let bodyFontSize = parseInt(getComputedStyle(this.body).fontSize, 10), calcFontSize = Math.floor(this.wrap.clientWidth / this.body.scrollWidth * bodyFontSize);
-            this.body.style.fontSize = `${calcFontSize}px`;
+            const bodyFontSize = parseInt(getComputedStyle(this.body).fontSize, 10);
+            const calcFontSize = Math.floor(this.wrap.clientWidth / this.body.scrollWidth * bodyFontSize);
+            Object.assign(this.wrap.style, {
+                display: "flex",
+                justifyContent: "center"
+            });
+            Object.assign(this.body.style, {
+                whiteSpace: "nowrap",
+                fontSize: `${calcFontSize}px`
+            });
         });
+    }
+    destroy() {
+        this.resizeObserver.unobserve(this.wrap);
+        document.fonts.removeEventListener("loadingdone", this.update);
+        this.af = null;
+        if (this.parentElement) {
+            Object.assign(this.wrap.style, {
+                display: "",
+                justifyContent: ""
+            });
+            Object.assign(this.body.style, {
+                fontSize: "",
+                whiteSpace: ""
+            });
+        }
     }
     connectedCallback() {
         this.update();
+        this.init();
     }
     disconnectedCallback() {
-        this.resizeObserver.unobserve(this.wrap);
-        document.fonts.removeEventListener("loadingdone", this.update);
+        this.destroy();
     }
 }
 customElements.get("text-fitting") || customElements.define("text-fitting", $cf838c15c8b009ba$export$2e2bcd8739ae039);
--- src/index.js
+++ src/index.js
@@ -4,17 +4,18 @@ export default class TextFitting extends HTMLElement {
 
 		const shadowOpen = this.attachShadow({ mode: 'open' })
 
-		shadowOpen.innerHTML = `<div class="wrap" style="display: flex; justify-content: center;"><div class="body" style="white-space: nowrap;"><slot></slot></div></div>`
+		shadowOpen.innerHTML = `<div class="wrap"><div class="body"><slot></slot></div></div>`
 
 		this.wrap = shadowOpen.querySelector('.wrap')
 		this.body = shadowOpen.querySelector('.body')
 		this.update = this.update.bind(this)
 		this.af = null
-
 		this.resizeObserver = new ResizeObserver(this.update)
-		this.resizeObserver.observe(this.wrap)
+	}
 
+	init() {
 		document.fonts.addEventListener('loadingdone', this.update)
+		this.resizeObserver.observe(this.wrap)
 	}
 
 	update() {
@@ -23,21 +24,32 @@ export default class TextFitting extends HTMLElement {
 		}
 
 		this.af = requestAnimationFrame(() => {
-			let bodyFontSize = parseInt(getComputedStyle(this.body).fontSize, 10),
-				calcFontSize = Math.floor((this.wrap.clientWidth / this.body.scrollWidth) * bodyFontSize)
+			const bodyFontSize = parseInt(getComputedStyle(this.body).fontSize, 10)
+			const calcFontSize = Math.floor((this.wrap.clientWidth / this.body.scrollWidth) * bodyFontSize)
 
-			this.body.style.fontSize = `${calcFontSize}px`
+			Object.assign(this.wrap.style, { display: 'flex', justifyContent: 'center' })
+			Object.assign(this.body.style, { whiteSpace: 'nowrap', fontSize: `${calcFontSize}px` })
 		})
+	}
+
+	destroy() {
+		this.resizeObserver.unobserve(this.wrap)
+		document.fonts.removeEventListener('loadingdone', this.update)
+		this.af = null
+
+		if (this.parentElement) {
+			Object.assign(this.wrap.style, { display: '', justifyContent: '' })
+			Object.assign(this.body.style, { fontSize: '', whiteSpace: '' })
+		}
 	}
 
 	connectedCallback() {
 		this.update()
+		this.init()
 	}
 
 	disconnectedCallback() {
-		this.resizeObserver.unobserve(this.wrap)
-
-		document.fonts.removeEventListener('loadingdone', this.update)
+		this.destroy()
 	}
 }
 
